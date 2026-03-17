d1030147b4eedd61114611bd27410d2523eaa75d
JEROME-1122 <jeromfranklin123@gmail.com>
2026-03-17 21:41:31 +0530

    text area added for description

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Button from "./components/ui/Button";
+import TextArea from "./components/ui/TextArea";
 import TextInput from "./components/ui/TextInput";
 
 function App() {
   return (
     <>
-      <div className="grid grid-cols-2 gap-4 p-4 items-center justify-center ">
+      <div className="grid grid-cols-3 gap-4 p-4 items-center justify-center ">
         <TextInput />
+        <TextArea />
         <Button />
       </div>
     </>
--- src/components/ui/TextArea.tsx
+++ src/components/ui/TextArea.tsx
@@ -1,5 +1,18 @@
+import React from "react";
+
 function TextArea() {
-  return <div>Text Area</div>;
+  const [value, setValue] = React.useState("");
+  return (
+    <div className="flex flex-col gap-1">
+      <label className="text-sm font-medium">Enter the Description </label>
+      <textarea
+        className="border p-2 rounded"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        placeholder="Enter Description  "
+      ></textarea>
+    </div>
+  );
 }
 
 export default TextArea;
