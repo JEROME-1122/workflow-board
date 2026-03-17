52fe4a3779ea440c192d34e3c14aed08f338b470
JEROME-1122 <jeromfranklin123@gmail.com>
2026-03-17 21:57:34 +0530

    toast added

--- src/components/ui/Toast.tsx
+++ src/components/ui/Toast.tsx
@@ -1,5 +1,19 @@
-function Toast() {
-  return <div>Toast component created</div>;
+type Props = {
+  message: string;
+  type?: "success" | "error";
+};
+
+function Toast({ message, type = "success" }: Props) {
+  const styles = {
+    success: "bg-green-500",
+    error: "bg-red-500",
+  };
+
+  return (
+    <div className={`text-white px-4 py-2 rounded ${styles[type]}`}>
+      {message}
+    </div>
+  );
 }
 
 export default Toast;
