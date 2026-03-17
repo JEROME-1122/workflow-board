06855f20493d3139960f288360dcefd219e422d7
JEROME-1122 <jeromfranklin123@gmail.com>
2026-03-17 21:45:51 +0530

    select section added and dummy information added in option using map

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import "./App.css";
 import Button from "./components/ui/Button";
+import Select from "./components/ui/Select";
 import TextArea from "./components/ui/TextArea";
 import TextInput from "./components/ui/TextInput";
 
 function App() {
   return (
     <>
-      <div className="grid grid-cols-3 gap-4 p-4 items-center justify-center ">
+      <div className="grid grid-cols-4 gap-4 p-4 items-center justify-center ">
         <TextInput />
+        <Select />
         <TextArea />
         <Button />
       </div>
--- src/components/ui/Select.tsx
+++ src/components/ui/Select.tsx
@@ -1,11 +1,26 @@
- 
-
 function Select() {
+  type Option = {
+    value: string;
+    label: string;
+  };
+  const options: Option[] = [
+    { value: "emp1", label: "Emp 1" },
+    { value: "emp2", label: "Emp 2" },
+    { value: "emp3", label: "Emp 3" },
+  ];
+
   return (
-    <div>
-      Select component created
+    <div className="flex flex-col gap-1">
+      <label className="text-sm font-medium">Select an Option</label>
+      <select className="border p-2 rounded">
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
     </div>
-  )
+  );
 }
 
-export default Select
+export default Select;
